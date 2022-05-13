2178ce4f411d495562cd1aee489b3805b72229bb
HbDevelop <boudriga.habib@gmail.com>
2022-05-13 12:20:08 +0200

    fix: fix timezone on notes

--- projects/angular-calendar/src/modules/week-list/calendar-week-list-view.component.ts
+++ projects/angular-calendar/src/modules/week-list/calendar-week-list-view.component.ts
@@ -767,7 +767,7 @@ export class CalendarWeekListViewComponent
           this.weekStartsOn,
           this.excludeDays,
           this.daysInWeek,
-          this.timezone
+          withTimezone ? this.timezone : null
         ),
       },
       withTimezone ? this.timezone : null
